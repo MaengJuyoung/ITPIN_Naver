f55d95fbb0b635c6cb4bb697e090f65a47a84704
Maeng JuYoung <wndud6302@naver.com>
2024-12-13 11:06:21 +0900

    container/shoppingTab 완료

---
+++ naverProject/bin/Source/SubView/shoppingTab.js
@@ -0,0 +1,38 @@
+
+/**
+Constructor
+Do not call Function in Constructor.
+*/
+function shoppingTab()
+{
+	AView.call(this);
+
+	//TODO:edit here
+
+}
+afc.extendsClass(shoppingTab, AView);
+
+
+shoppingTab.prototype.init = function(context, evtListener)
+{
+	AView.prototype.init.call(this, context, evtListener);
+
+	//TODO:edit here
+
+};
+
+shoppingTab.prototype.onInitDone = function()
+{
+	AView.prototype.onInitDone.call(this);
+
+	//TODO:edit here
+
+};
+
+shoppingTab.prototype.onActiveDone = function(isFirst)
+{
+	AView.prototype.onActiveDone.call(this, isFirst);
+
+	//TODO:edit here
+
+};
